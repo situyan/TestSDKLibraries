573793c5f9ccdbe1e16b66095b79c0573ed79b2a
Hank.Shi <hank.shi@chisalsoft.com>
2019-05-15 11:44:14 +0800

    first commit

---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoogleWebRTCGetDemo
+//
+//  Created by shi_mhua on 2019/5/14.
+//  Copyright © 2019 chisalsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  GoogleWebRTCGetDemo
+//
+//  Created by shi_mhua on 2019/5/14.
+//  Copyright © 2019 chisalsoft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
